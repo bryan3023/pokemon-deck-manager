c26b3ddbd43cf8ee52312146e37369f4b86b9b03
Bryan <bryan.fennell@hotmail.com>
2020-05-18 08:21:42 -0400

    Added search API, database population.

--- models/pokemon_model.js
+++ models/pokemon_model.js
@@ -1,13 +1,49 @@
 const
-  Pokedex = require('pokedex-promise-v2'),
-  PokemonTCG = require('pokemontcgsdk')
+  PokeAPI = require('pokedex-promise-v2'),
+  database = require('../config/database')
+
+const KANTO_NUM_POKEMON = 151
 
 class PokemonModel {
 
   constructor() {
-    this.pokedex = new Pokedex()
+    this.pokeAPI = new PokeAPI()
+    this.loadPokedex()
   }
 
+  async loadPokedex() {
+    const pokedex = await database.query('SELECT * FROM pokemon')
+
+    if (pokedex.length !== KANTO_NUM_POKEMON) {
+      const kanto = await this.getPokedex()
+
+      for (let pokemon_key of kanto) {
+
+        const pokemon = await this.getByKey(pokemon_key)
+        console.log(pokemon)
+        await database.query(`INSERT INTO pokemon SET ?`, {
+          pokemon_key: pokemon_key,
+          pokemon_name: pokemon.name,
+          pokemon_num: pokemon.number
+        })
+      }
+    }
+  }
+
+  async search(searchTerm) {
+    const santizedSearch = searchTerm.trim()
+    let queryString, queryMatch
+
+    if (parseInt(searchTerm)) {
+      queryString = 'SELECT * FROM pokemon WHERE pokemon_num = ?'
+      queryMatch = santizedSearch
+    } else {
+      queryString = 'SELECT * FROM pokemon WHERE pokemon_name LIKE ?'
+      queryMatch = `%${santizedSearch}%`
+    }
+
+    return await database.query(queryString, queryMatch)
+  }
 
   async getPokemonCardByName(name) {
       return await PokemonTCG.card.where({
@@ -17,11 +53,16 @@ class PokemonModel {
       })
   }
 
-  async getPokemonByName(name) {
+  async getAll() {
+    const names = await database.query('SELECT pokemon_name FROM pokemon')
+    return names.map(n => n.pokemon_name)
+  }
+
+  async getByKey(name) {
     try {
       const
-        pokemon = await this.pokedex.getPokemonByName(name),
-        pokemonSpecies = await this.pokedex.getPokemonSpeciesByName(name),
+        pokemon = await this.pokeAPI.getPokemonByName(name),
+        pokemonSpecies = await this.pokeAPI.getPokemonSpeciesByName(name),
 
         flavorText = pokemonSpecies.flavor_text_entries
           .filter(f => f.language.name === "en")
@@ -32,6 +73,8 @@ class PokemonModel {
         pokemonAbilities = pokemon.abilities.map(a => a.ability.name),
         pokemonMoves = pokemon.moves.map(m => m.move.name),
         pokemonTypes = pokemon.types.map(t => t.type.name)
+
+//        console.log(await this.pokeAPI.getMovesList(pokemonMoves))
 
         return {
           name: pokemonName,
@@ -60,10 +103,8 @@ class PokemonModel {
 
   async getPokedex() {
     const
-      pokedex = await this.pokedex.getPokedexByName('kanto'),
+      pokedex = await this.pokeAPI.getPokedexByName('kanto'),
       pokemonName = pokedex.pokemon_entries.map(p => p.pokemon_species.name)
-
-      console.log(pokemonName)
 
     return pokemonName;
   }
